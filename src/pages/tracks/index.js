384123e408ce127e7659fbcd69e63f42a42c16ad
Nigel Wade <nigeljohnwade@gmail.com>
2021-10-02 12:25:41 +0000

    Merged in development (pull request #5)
    Development
    
    * Fonts, typography, background image
    
    * Add font files
    
    * Heading drop shadow
    
    * Merged in TP-2-home-page-content (pull request #1)
    
    TP-2 home page content
    
    * Fonts, typography, background image
    
    * Add font files
    
    * Heading drop shadow
    
    * Add navigation
    
    * Fluid styles
    * Merge remote-tracking branch 'bitbucket/development' into development
    
    # Conflicts:
    #	src/components/atoms/Typography/Typography.jsx
    #	src/components/atoms/Typography/Typography.module.css
    #	src/css/styles.css
    #	src/pages/_app.js
    #	src/pages/index.js
    
    * Merged in TP-3-tracks (pull request #3)
    
    TP-3 tracks
    
    * Add navigation
    
    * Fluid styles
    
    * Track data
    
    * Tidy up
    
    * More tidy up
    
    * Typos

---
+++ src/pages/tracks/index.js
@@ -0,0 +1,62 @@
+import React, { Fragment } from 'react';
+import Typography from '../../components/atoms/Typography/Typography';
+import Navigation from '../../components/molecules/Navigation/Navigation';
+import Page from '../../components/layout/Page/Page';
+
+const TracksPage = ({
+    headerData,
+    trackData,
+}) => {
+    console.log(trackData, headerData);
+    return (
+        <Page>
+            <Typography
+                element="h1"
+                variant="h1"
+                modifier="lowercase"
+            >
+                Tracks
+            </Typography>
+            <Navigation/>
+            {trackData && trackData.length > 0 && trackData.map(track => {
+                return (
+                    <Fragment key={track[2]}>
+                        <Typography
+                            element="h2"
+                            variant="h2"
+                        >
+                            {track[0]}
+                        </Typography>
+                        <Typography
+                            element="p"
+                        >
+                            {track[1]}
+                        </Typography>
+                        <Typography>
+                            {track[2]}
+                        </Typography>
+                    </Fragment>
+                );
+            })
+
+            }
+        </Page>
+    );
+};
+
+export default TracksPage;
+
+export async function getStaticProps() {
+    const data = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTB6b_fWUvjJStAnbO-147rRk9qgWk6RBc0UntZLE9dHIC6ituaDErw1CKgnN7a6sqMUYIZAF7jdWiT/pub?gid=0&single=true&output=tsv')
+        .then(response => response.text())
+        .then(rawData => rawData.split('\r\n'))
+        .then(data => data.map(item => item.split('\t')));
+    const headerData = data.splice(0, 1);
+    return {
+        props: {
+            trackData: data,
+            headerData,
+            revalidate: 300,
+        }
+    };
+}
