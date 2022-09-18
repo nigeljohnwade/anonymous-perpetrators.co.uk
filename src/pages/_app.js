590e5bd53603919480227354507eb9531eb93101
Nigel Wade <nigeljohnwade@gmail.com>
2022-09-18 17:22:02 +0100

    Move mete tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,14 @@
 import 'css/styles.css';
 import 'css/media-queries.css';
+import Head from 'next/head';
 
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+export default function MyApp({Component, pageProps}) {
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"/>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"/>
+
             </Head>
             <body>
             <Main />
