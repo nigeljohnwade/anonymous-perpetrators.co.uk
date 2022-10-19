98ac2f6dcd1ad79f198401eb439ec67b7b650e43
Nigel Wade <nigeljohnwade@gmail.com>
2022-10-19 09:56:41 +0100

    Add stat counter

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,6 +14,20 @@ export default function Document() {
             <body>
             <Main />
             <NextScript />
+            <div className="statcounter">
+                <a
+                    title="Web Analytics Made Easy - Statcounter"
+                    href="https://statcounter.com/"
+                    target="_blank"
+                >
+                    <img
+                        className="statcounter"
+                        src="https://c.statcounter.com/12807722/0/85f40b8a/1/"
+                        alt="Web Analytics Made Easy - Statcounter"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    />
+                </a>
+            </div>
             </body>
         </Html>
     )
