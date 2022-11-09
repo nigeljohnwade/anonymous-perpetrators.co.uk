6d3175c786bbdbcff2745f6a2ca85b35f005b016
Nigel Wade <nigeljohnwade@gmail.com>
2022-11-09 20:38:07 +0000

    Update packages

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,10 +12,20 @@ const HomePage = () => {
                 display: 'flex',
                 flexDirection: 'row',
                 justifyContent: 'flex-end',
+                gap: 'var(--gutter)',
             }}>
                 <a href="https://promocards.byspotify.com/share/162ce1d96336e8c0a5d467f66e33bd208fb740f3">
                     <img
                         src="/promo_card_spotify_162ce1d96336e8c0a5d467f66e33bd208fb740f3.png"
+                        style={{
+                            width: '45vh',
+                            height: '45vh',
+                        }}
+                    />
+                </a>
+                <a href="https://soundcloud.com/anonymousperpetrators">
+                    <img
+                        src="/promo_card_soundcloud.png"
                         style={{
                             width: '45vh',
                             height: '45vh',
