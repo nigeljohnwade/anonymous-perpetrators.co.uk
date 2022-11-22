8529189b01a49befda63559576d088534d7b21b6
Nigel Wade <nigeljohnwade@gmail.com>
2022-11-22 21:13:17 +0000

    Add keystep pro

--- src/components/layout/Page/Page.jsx
+++ src/components/layout/Page/Page.jsx
@@ -9,20 +9,30 @@ const Page = ({
 }) => {
 
     return (
-        <div
-            className={styles.page}
-            style={{
-                backgroundColor:  backgroundColor,
-                backgroundImage: backgroundImage,
-                backgroundSize: backgroundSize,
-                backgroundRepeat: 'no-repeat',
-                backgroundPositionX: 'left',
-                backgroundPositionY: 'top',
-            }}
-        >
-            {children}
-        </div>
-    )
+        <>
+            {/*<div className={styles['sky-gradient']}>*/}
+            {/*</div>*/}
+            {/*<div className={styles['ground-plane']}>*/}
+            {/*    <div className={styles['vapor-grid-glow']}>*/}
+            {/*    </div>*/}
+            {/*    <div className={styles['vapor-grid']}>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <div
+                className={styles.page}
+                style={{
+                    backgroundColor: backgroundColor,
+                    backgroundImage: backgroundImage,
+                    backgroundSize: backgroundSize,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPositionX: 'left',
+                    backgroundPositionY: 'top',
+                }}
+            >
+                {children}
+            </div>
+        </>
+    );
 };
 
 export default Page;
