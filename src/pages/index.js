80f5806d58f21492d512a5b0628f4b2301612084
Nigel Wade <nigeljohnwade@gmail.com>
2022-12-09 12:55:51 +0000

    Home page scrolling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Page from 'components/layout/Page/Page';
 import Header from 'components/organisms/Header/Header';
+import CardDeck from 'components/molecules/CardDeck/CardDeck';
 
 const HomePage = () => {
     return (
@@ -10,12 +11,7 @@ const HomePage = () => {
             backgroundSize={'cover'}
         >
             <Header pageTitle="Anonymous Perpetrators"/>
-            <main style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'flex-end',
-                gap: 'var(--gutter)',
-            }}>
+            <CardDeck additionalStyles={{'flexDirection': 'flex-end'}}>
                 <a href="https://promocards.byspotify.com/share/162ce1d96336e8c0a5d467f66e33bd208fb740f3">
                     <img
                         src="/promo_card_spotify_162ce1d96336e8c0a5d467f66e33bd208fb740f3.png"
@@ -34,7 +30,7 @@ const HomePage = () => {
                         }}
                     />
                 </a>
-            </main>
+            </CardDeck>
         </Page>
     );
 };
