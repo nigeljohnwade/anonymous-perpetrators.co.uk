2d8b7637c1cd2c831a0083de9faf896a33bd7363
Nigel Wade <nigeljohnwade@gmail.com>
2023-12-05 12:28:01 +0000

    Typo

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,7 +15,7 @@ const HomePage = () => {
                 <Typography element="p">Making electronic music since the mid nineties.</Typography>
                 <Typography element="p">The name came from a speech bubble in 200AD, though not verbatim, which was de rigor for the time. I
                     have not read much 2000AD at all.</Typography>
-                <Typography element="p">Primary software is Reason (don't @ me) which I absolutley love for it's creative versatility.</Typography>
+                <Typography element="p">Primary software is Reason (don't @ me) which I absolutely love for its creative versatility.</Typography>
                 <Typography element="p">Also use an Arturia Keystep as a sequencer a lot providing a link between Reason/Midi and some
                     CV/Gate/Curve hardware.</Typography>
             </div>
