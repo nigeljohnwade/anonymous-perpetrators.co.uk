1883eb0ab817f15f9213cd4273e5611be16b52dd
Nigel Wade <nigel.wade@cyviz.com>
2024-12-10 12:36:20 +0000

    Devices from json data file

--- src/pages/devices/[device]/index.js
+++ src/pages/devices/[device]/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
+import data from 'data/AnonymousPerpetratorsDevices.json';
 import DeviceCard from 'components/organisms/DeviceCard/DeviceCard';
 import Page from 'components/layout/Page/Page';
 import Header from 'components/organisms/Header/Header';
 
-const TracksPage = ({
-    headerData,
-    deviceData,
-}) => {
+const TracksPage = () => {
     const router = useRouter();
     const deviceName = router.query.device;
-    const deviceItemData = deviceData.filter(item => {
-        return item[0] === deviceName;
+    const deviceItemData = data.filter(item => {
+        return item['Device Stub'] === deviceName;
     })[0];
-    const backgroundColor = deviceItemData[7] !== '' ? deviceItemData[7] : undefined;
-    const backgroundImage = deviceItemData[1] !== '' ? `url(/${deviceItemData[1]})` : undefined;
+    // const backgroundColor = deviceItemData[7] !== '' ? deviceItemData[7] : undefined;
+    const backgroundImage = deviceItemData['Device Main Image'] !== '' ? `url(/${deviceItemData['Device Main Image']})` : undefined;
+    const backgroundColor = 'black';
 
     return (
         <Page
@@ -23,14 +22,14 @@ const TracksPage = ({
             backgroundImage={backgroundImage}
             backgroundSize={'100vh'}
         >
-            <Header pageTitle={deviceItemData[0]}/>
+            <Header pageTitle={deviceItemData['Device Name']} />
             <div
                 style={{
                     flexBasis: '50%',
                 }}
             >
                 <DeviceCard
-                    key={deviceItemData[1]}
+                    key={deviceItemData['Device Name']}
                     deviceItemData={deviceItemData}
                 />
             </div>
@@ -41,7 +40,7 @@ const TracksPage = ({
 export default TracksPage;
 
 export async function getServerSideProps() {
-    const deviceData =  await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTB6b_fWUvjJStAnbO-147rRk9qgWk6RBc0UntZLE9dHIC6ituaDErw1CKgnN7a6sqMUYIZAF7jdWiT/pub?gid=643050921&output=tsv')
+    const deviceData = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTB6b_fWUvjJStAnbO-147rRk9qgWk6RBc0UntZLE9dHIC6ituaDErw1CKgnN7a6sqMUYIZAF7jdWiT/pub?gid=643050921&output=tsv')
         .then(response => response.text())
         .then(rawData => rawData.split('\r\n'))
         .then(data => data.map(item => item.split('\t')));
--- src/pages/devices/index.js
+++ src/pages/devices/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
+
+import data from 'data/AnonymousPerpetratorsDevices.json';
 import Page from 'components/layout/Page/Page';
 import Header from 'components/organisms/Header/Header';
 import DeviceLinkCard from 'components/organisms/DeviceLinkCard/DeviceLinkCard';
 import CardDeck from 'components/molecules/CardDeck/CardDeck';
 
-const DevicesPage = ({
-    deviceData,
-}) => {
+const DevicesPage = () => {
     return (
         <Page
             backgroundImage={'url(/vapor_grid.png)'}
@@ -16,10 +16,10 @@ const DevicesPage = ({
             <Header pageTitle="Devices"/>
             <CardDeck>
                 {
-                    deviceData && deviceData.length > 0 && deviceData.map(device => {
+                    data && data.length > 0 && data.map(device => {
                         return (
                             <DeviceLinkCard
-                                key={device[0]}
+                                key={device['DeviceName']}
                                 device={device}
                             />
                         );
@@ -31,19 +31,3 @@ const DevicesPage = ({
 };
 
 export default DevicesPage;
-
-export async function getStaticProps() {
-    const deviceData = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTB6b_fWUvjJStAnbO-147rRk9qgWk6RBc0UntZLE9dHIC6ituaDErw1CKgnN7a6sqMUYIZAF7jdWiT/pub?gid=643050921&output=tsv')
-        .then(response => response.text())
-        .then(rawData => rawData.split('\r\n'))
-        .then(data => data.map(item => item.split('\t')));
-    const headerData = deviceData.splice(0, 1);
-
-    return {
-        props: {
-            deviceData,
-            headerData,
-            revalidate: 300,
-        }
-    };
-}
